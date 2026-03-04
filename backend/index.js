3320181c8bf01286605181d3141d1a3b179ab6d1
HMAjay <ajaymanjunath25@gmail.com>
2026-03-04 13:48:00 +0530

    Student HTML ready

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ import {connectDB} from './config/db.js';
 import recruiterRoutes from "./routes/recruiter.route.js"
 import studentRoutes from "./routes/student.route.js";
 import { initiateInvites } from "./controllers/utils.controller.js";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT;
 const app=express(); 
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/auth/students",studentRoutes);
 app.use("/auth/recruiters",recruiterRoutes);
