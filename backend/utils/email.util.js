c697abd21e983d5cb581755efe7723b7ff7c3fd3
HMAjay <ajaymanjunath25@gmail.com>
2026-04-27 14:04:15 +0530

    Ajay pushed to main

--- backend/utils/email.util.js
+++ backend/utils/email.util.js
@@ -5,6 +5,7 @@ export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_PORT === "465",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
